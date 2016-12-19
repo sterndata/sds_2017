5973a0a16d12081610d410fffeca5910bebfc70d
Steven Stern <steve@sterndata.com>
2016-12-19 12:38:58 -0600

    modify images to 600px high instead of 1200

--- functions.php
+++ functions.php
@@ -6,3 +6,18 @@ function my_theme_enqueue_styles() {
     wp_enqueue_script( 'jquery' );
 
 }
+/**
+ * change the recommended header image size to 600px high
+**/
+
+function sds_2017_twentyseventeen_custom_header_args( $args ) {
+   $args['height'] = 600;
+   return $args;
+   }
+add_filter ( 'twentyseventeen_custom_header_args', 'sds_2017_twentyseventeen_custom_header_args' );
+
+/**
+ * override the special featured image to be 600 high instead of 1200
+** /
+
+add_image_size( 'twentyseventeen-featured-image', 2000, 600, true );
