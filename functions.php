313d1515347b97725efdb3714dea9d5ca9273359
Steven Stern <steve@sterndata.com>
2016-12-19 12:53:47 -0600

    codesniffer format fixes

--- functions.php
+++ functions.php
@@ -2,8 +2,8 @@
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 function my_theme_enqueue_styles() {
 
-    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
-    wp_enqueue_script( 'jquery' );
+	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+	wp_enqueue_script( 'jquery' );
 
 }
 /**
@@ -11,15 +11,15 @@ function my_theme_enqueue_styles() {
 **/
 
 function sds_2017_twentyseventeen_custom_header_args( $args ) {
-   $args['height'] = 600;
-   return $args;
-   }
-add_filter ( 'twentyseventeen_custom_header_args', 'sds_2017_twentyseventeen_custom_header_args' );
+	$args['height'] = 600;
+	return $args;
+}
+add_filter( 'twentyseventeen_custom_header_args', 'sds_2017_twentyseventeen_custom_header_args' );
 
 /**
  * override the special featured image to be 600 high instead of 1200
 **/
 function sds_2017_overide_featured_image() {
-   add_image_size( 'twentyseventeen-featured-image', 2000, 600, true );
+	add_image_size( 'twentyseventeen-featured-image', 2000, 600, true );
 }
 add_action( 'after_setup_theme', 'sds_2017_overide_featured_image', 11 );
