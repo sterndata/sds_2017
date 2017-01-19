95ac132987f91330f3ec6cce5ccf0eb196400e01
Steven Stern <steve@sterndata.com>
2017-01-18 23:33:34 -0600

    use Bitter for header font

--- functions.php
+++ functions.php
@@ -3,6 +3,7 @@ add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 function my_theme_enqueue_styles() {
 
 	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+        wp_enqueue_style( 'sds_2017_google_fonts', 'https://fonts.googleapis.com/css?family=Bitter' );
 
 }
 /**
